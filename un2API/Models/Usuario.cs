a04cdef8697933ef6f5b574ffc3c577f39eecd75
Ipojuca Moraes da Cruz <ipojuca@expertsinfo.com.br>
2022-06-14 18:21:36 -0300

    Publicação para entrega ao professor

---
+++ un2API/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace un2API.Models
+{
+
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+
+        //public ICollection<Tarefa>? Tarefas { get; set; }
+    }
+}
